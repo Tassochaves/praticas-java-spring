fa40c08a14ff446e4d7a95fa625f1fa32e99987e
Tassochaves <tassogt@hotmail.com>
2025-11-04 22:44:20 -0300

    first commit

---
+++ src/main/java/com/dev/praticas_spring/PraticasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dev.praticas_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/praticas_spring/PraticasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.praticas_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
